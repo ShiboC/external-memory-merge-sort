ca5f11307af3ae778e50cd7922b5a6c331bd32aa
ShiboC <shibocheng33@foxmail.com>
2016-11-11 16:24:43 +0100

    check

--- src/IOStream4.java
+++ src/IOStream4.java
@@ -32,30 +32,8 @@ public class IOStream4 {
 
 
 	public static void main(String[] args) throws IOException {
-		int count=10;
-		// TODO Auto-generated method stub
-        RandomAccessFile memoryMappedFile = new RandomAccessFile("./src/largeFile.txt", "rw");
+		
 
-
-        // Mapping a file into memory
-        MappedByteBuffer out = memoryMappedFile.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, count);
-
-
-        // Writing into Memory Mapped File
-        for (int i = 0; i < count; i++) {
-            out.put((byte) 'B');
-        }
-        System.out.println("Writing to Memory Mapped File is completed");
-
-
-        // reading from memory file in Java
-        for (int i = 0; i < 10; i++) {
-            System.out.print((char) out.get(i));
-        }
-        System.out.println("Reading from Memory Mapped File is completed");
-
-
-        memoryMappedFile.close();
 	}
 
 }
