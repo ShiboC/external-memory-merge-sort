896a4e01f0bed34273623c0031ec0364c91491f8
Pandu Wicaksono <pandu.wicaksono91@gmail.com>
2016-11-11 16:09:21 +0100

    debugging

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 		public static void main(String[] args)
 		{
 			IOStream1 ioStream1 = new IOStream1();
-			ioStream1.readFile("input.data");
+			ioStream1.readFile("input2.data");
 			
 		}
 }
