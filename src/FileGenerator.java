144ba9b6908d949e6289815b18978dcc11aa0a02
martina-zxy <martina.megasari@gmail.com>
2016-11-11 15:38:50 +0100

    Renaming variable

--- src/FileGenerator.java
+++ src/FileGenerator.java
@@ -4,15 +4,15 @@ import java.io.UnsupportedEncodingException;
 
 
 public class FileGenerator {
-	private int elementNo;
+	private int nbElement;
 	
 	public FileGenerator(int element) {
-		this.elementNo = element;
+		this.nbElement = element;
 	}
 	
 	public void generateFile() throws FileNotFoundException, UnsupportedEncodingException{
 	    PrintWriter writer = new PrintWriter("input.data", "UTF-8");
-	    for(int i = 0; i < elementNo; i++) {
+	    for(int i = 0; i < nbElement; i++) {
 	    	writer.println((int)(Math.random() * Integer.MAX_VALUE));
 	    }
 	    writer.close();
