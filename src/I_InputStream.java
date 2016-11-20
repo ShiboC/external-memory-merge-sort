00404e4a81bad12370b36d4a8d91b57b18067f73
martina-zxy <martina.megasari@gmail.com>
2016-11-20 15:18:08 +0100

    IOStream2_Input.java Input Interface open function specify parameter

--- src/I_InputStream.java
+++ src/I_InputStream.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public interface I_InputStream {
 	
-	public void open() throws IOException;
+	public void open(String filename) throws IOException;
 	public boolean end_of_stream() throws IOException;
 	public int read_next() throws IOException;
 	
