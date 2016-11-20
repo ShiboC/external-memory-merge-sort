004d471119066f3b7f712c4efd188ec1d8572734
ShiboC <shibocheng33@foxmail.com>
2016-11-20 18:15:33 +0100

    add number of elements to write in type4

--- src/IOStream4_Output.java
+++ src/IOStream4_Output.java
@@ -4,25 +4,28 @@ import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
-public class IOStream4_output implements I_OutputStream {
+public class IOStream4_Output implements I_OutputStream {
 	private RandomAccessFile randomAccessFile;
 	private FileChannel fileChannel;
 	// the 'B' element portion of file in the assignment description
 	private int bufferSize;
 	// the current position in the file that need to be mapped
-	private long position;
+	private int position;
 	// A direct byte buffer whose content is a memory-mapped region of a file
 	private MappedByteBuffer mappedByteBuffer;
 	private String filePath;
+	// specify the number of elements to write
+	private int noToWrite;
 
-	public IOStream4_output(String filePath, int bElements) throws IOException {
+	public IOStream4_Output(String filePath, int bElements, int noToWrite) throws IOException {
 		this.bufferSize = bElements * 4;
 		this.position = 0;
 		this.filePath = filePath;
+		this.noToWrite = noToWrite;
 	}
-	
+
 	// map the file into the memory
-	private void map() throws IOException {
+	private void map(int bufferSize) throws IOException {
 		mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, position, bufferSize);
 		position += bufferSize;
 	}
@@ -30,13 +33,14 @@ public class IOStream4_output implements I_OutputStream {
 	@Override
 	public void write(int value) throws IOException {
 		if (!mappedByteBuffer.hasRemaining()) {
-			map();
+			map(Math.min(bufferSize, noToWrite*4-position));
 		}
 		mappedByteBuffer.putInt(value);
 	}
 
 	@Override
 	public void close() throws IOException {
+
 		randomAccessFile.close();
 	}
 
@@ -44,7 +48,8 @@ public class IOStream4_output implements I_OutputStream {
 	public void create() throws IOException {
 		this.randomAccessFile = new RandomAccessFile(filePath, "rw");
 		this.fileChannel = randomAccessFile.getChannel();
-		map();
+		
+		map(Math.min(bufferSize, noToWrite*4));
 	}
 
 }
