d95314bb4b71f223e54976ebc8dedc1823df434a
Pandu Wicaksono <pandu.wicaksono91@gmail.com>
2016-11-20 14:13:06 +0100

    IOStream1 commit

--- src/IOStream1.java
+++ src/IOStream1.java
@@ -1,8 +1,11 @@
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class IOStream1 {
 	public void readFile(String fileName)
@@ -11,11 +14,18 @@ public class IOStream1 {
 		InputStream is = new FileInputStream(new File (fileName));
 		DataInputStream ds = new DataInputStream(is);
 		
+//		String s = ds.readLine();
+//		start time
+		// read here
 		while(ds.available() > 0){
 			int a  = ds.readInt();
 			System.out.println(a);
 		}
 		
+		// write here
+		
+		// end time
+		// calculate time = end time - start time
 		// close the stream
 		ds.close();
 		
@@ -23,5 +33,25 @@ public class IOStream1 {
 		{
 			// do nothing
 		}
+	}
+	
+	public void writeFile(String fileName)
+	{
+		try
+		{
+			OutputStream os = new FileOutputStream(new File(fileName));
+			DataOutputStream ds = new DataOutputStream(os);
+			
+			for(int ii = 0; ii <= 1000000; ii++)
+				ds.writeInt(ii);
+			
+			
+		}
+		catch (IOException e)
+		{
+			
+			
+		}
+		
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
 		public static void main(String[] args)
 		{
 			IOStream1 ioStream1 = new IOStream1();
-			ioStream1.readFile("input2.data");
+			ioStream1.writeFile("input3.data");
+			ioStream1.readFile("input3.data");
+			
 			
 		}
 }
