c014a9a96dbbf3651641258272dac30a0e09d003
martina-zxy <martina.megasari@gmail.com>
2016-11-20 15:02:47 +0100

    IOStream2

---
+++ src/IOStream2_Input.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+public class IOStream2_Input implements I_InputStream {
+
+	@Override
+	public void open() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean end_of_stream() throws IOException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int read_next() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/IOStream2_Output.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+public class IOStream2_Output implements I_OutputStream {
+
+	@Override
+	public void create() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void write(int data) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
