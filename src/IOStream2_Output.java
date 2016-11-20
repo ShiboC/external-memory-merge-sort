bf607f36b7c32c08e6ef0a98b8d49291d66e0cb8
martina-zxy <martina.megasari@gmail.com>
2016-11-20 16:14:51 +0100

    IOStream2_output

--- src/IOStream2_Output.java
+++ src/IOStream2_Output.java
@@ -1,23 +1,46 @@
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class IOStream2_Output implements I_OutputStream {
 
+	private OutputStream os;
+	private BufferedOutputStream bos;
+	private DataOutputStream ds;
+	private String filename;
+	
+	public IOStream2_Output(String filename){
+		this.filename = filename;
+	}
+	
 	@Override
 	public void create() throws IOException {
-		// TODO Auto-generated method stub
-
+		os = new FileOutputStream( new File(filename) );
+		bos = new BufferedOutputStream( os );
+		ds = new DataOutputStream( bos );
 	}
 
 	@Override
 	public void write(int data) throws IOException {
-		// TODO Auto-generated method stub
-
+		ds.writeInt(data);
 	}
 
 	@Override
 	public void close() throws IOException {
-		// TODO Auto-generated method stub
-
+		try {
+			if(ds != null) ds.close();
+		} catch (IOException e) {
+			
+		}
+		
+		try {
+			if(os != null) os.close();
+		} catch (IOException e) {
+			
+		}		
 	}
 
 }
